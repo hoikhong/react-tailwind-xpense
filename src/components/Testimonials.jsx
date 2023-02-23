aad0bfa1b0c4cdbfb7245faa1a3b225339819526
ipoobest <poobest.pooh@gmail.com>
2023-02-23 19:39:14 +0700

    testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+// import data
+import { testimonials } from "@/data.js";
+// import components
+import ClientSlider from "@/components/ClientSlider";
 function Testimonials() {
-  return <div>Testimonials</div>;
+  // destructure testimonials
+  const { title, clients } = testimonials;
+  return (
+    <section className="section">
+      <div
+        className="container mx-auto"
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
+        {/* title */}
+        <h2 className="title mb-10 lg:mb:20 text-center max-w-[920px] mx-auto">
+          {title}
+        </h2>
+        {/* slider */}
+        <div data-aos="fade-up" data-aos-delay="400">
+          <ClientSlider clients={clients} />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Testimonials;
