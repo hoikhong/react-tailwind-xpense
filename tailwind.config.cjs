88651a2638b1e180fc2a502b0ed74d8e3ea12ba3
ipoobest <poobest.pooh@gmail.com>
2023-02-23 13:28:16 +0700

    hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,32 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      primary: "Hind Siliguri",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        lg: "30px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1170px",
+    },
     extend: {
       colors: {
-        primary: "#FF7235",
+        dark: "#292830",
+        light: "#BDBDBD",
+        accent: "#FF7235",
+        accentHover: "#e05216",
+        grey: "#F5F5F5",
+      },
+      backgroundImage: {
+        overview: "url('/src/assets/img/overview/bg.svg')",
+        cta: "url('/src/assets/img/cta/bg.svg')",
       },
     },
   },
